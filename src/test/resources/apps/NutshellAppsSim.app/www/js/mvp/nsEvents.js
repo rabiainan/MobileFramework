22b66a1704c79ad257261c3575ed4685288d7952
Rabia <r_inan@hotmail.com>
2021-03-20 13:15:40 +0000

    MOBILE FRAMEWORK Changes

--- src/test/resources/apps/NutshellAppsSim.app/www/js/mvp/nsEvents.js
+++
@@ -1,6 +0,0 @@
-const NsEvents = {
-    SYNC_ALL: 'sync all',
-    SYNC_FINISHED: 'sync finished',
-    REQ_SET_DOMAIN: 'request set domain',
-    SCREEN_READY: 'screen ready'
-};
--- src/test/resources/apps/NutshellAppsSim.app/www/js/utils/validator.js
+++
@@ -1,4 +0,0 @@
-function isAuthFail(error) {
-    console.error("Validator: error", error);
-    return typeof error === 'string' && error.indexOf('Authentication failure') >= 0;
-}
